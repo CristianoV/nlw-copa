5cc383136947d010bfe590d7168306ad5bc01189
CristianoV <cristianoviieira@gmail.com>
2022-11-14 02:59:39 -0300

    feat: novas rotas criadas

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
+import {z} from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -20,7 +22,38 @@ async function bootstrap() {
     return { count };
   });
 
-  await fastify.listen({ port: 3001 });
+  fastify.get('/users/count', async (request, reply) => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  fastify.get('/guesses/count', async (request, reply) => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    const pool = await prisma.pool.create({
+      data: {
+        title,
+        code
+      },
+    });
+
+    return reply.status(201).send({ code })
+
+  });
+
+  await fastify.listen({ port: 3333 });
 }
 
 bootstrap();
