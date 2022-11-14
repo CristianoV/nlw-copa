7a5804a3cb4b5bbb88911bd235ffd1dd243773c0
CristianoV <cristianoviieira@gmail.com>
2022-11-14 03:07:26 -0300

    feat: nova configuração de status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { NativeBaseProvider} from "native-base";
+import { NativeBaseProvider, StatusBar} from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
-import { Signin } from './src/screens/Signin';
+import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/Components/Loading';
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold
-  });
-
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+  
   return (
     <NativeBaseProvider theme={THEME}>
-        {fontsLoaded ? <Signin/> : <Loading/>}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
